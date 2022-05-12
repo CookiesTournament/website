f477e4e82ac38c6f288e433fe4a5cee19a4bd7de
Lenny Angst <lenny@familie-angst.ch>
2022-05-12 19:18:44 +0200

    💄 Fix unnecessary scroll thingy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Book, Discord, HouseDoor, Info, Pen } from "react-bootstrap-icons";
-import Link from "next/link";
 import { NavItem } from "../components/NavItem";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <header className="sticky flex justify-center bg-slate-900 text-slate-50 text-xl p-2 font-bold shadow">
+      <header className="flex justify-center bg-slate-900 text-slate-50 text-xl p-2 font-bold shadow">
         <nav className="flex justify-between gap-7">
           <NavItem href="/" icon={<HouseDoor />} label="Home" />
           <NavItem href="/infos" icon={<Info />} label="Infos" />
@@ -19,7 +18,7 @@ function App({ Component, pageProps }: AppProps) {
           />
         </nav>
       </header>
-      <main className="p-5 text-slate-50 bg-slate-800 min-h-screen">
+      <main className="p-5 text-slate-50">
         <Component {...pageProps} />
       </main>
     </>
